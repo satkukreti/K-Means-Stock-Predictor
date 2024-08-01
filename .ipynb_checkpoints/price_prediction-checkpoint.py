5fa846d1e555da6ed108538df50f05a65f17b2ec
satkukreti <satkukreti@gmail.com>
2024-08-01 11:15:29 -0400

    Added technical indicators for clustering

--- .ipynb_checkpoints/price_prediction-checkpoint.py
+++ .ipynb_checkpoints/price_prediction-checkpoint.py
@@ -24,27 +24,11 @@ df = yf.download(tickers=symbols_list, start=start_date, end=end_date)
 df = df.stack() # multi indexed
 
 df.index.names = ['date', 'ticker']
-df.columns = df.columns.str.lower()
+df.columns = df.columns.tolower()
 
 # calculate technical indicators
 # Garman-Klass Volatility, RSI, Bollinger Bands, ATR, MACD, Dollar Volume
 
 df['garman-klass_vol'] = ((np.log(df['high'])-np.log(df['low']))**2)/2 - (2*np.log(2) - 1)*((np.log(df['adj close'])) - np.log(df['open'])**2)
 df['rsi'] = df.groupby(level=1)['adj close'].transform(lambda x : pandas_ta.rsi(close=x, length=20))
-df['bb_low'] = df.groupby(level=1)['adj close'].transform(lambda x : pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,0])
-df['bb_mid'] = df.groupby(level=1)['adj close'].transform(lambda x : pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,1])
-df['bb_high'] = df.groupby(level=1)['adj close'].transform(lambda x : pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,2])
-
-def compute_atr(stock_data):
-    atr = pandas_ta.atr(high=stock_data['high'], low=stock_data['low'], close=stock_data['close'], length=14)
-    return atr.sub(atr.mean()).div(atr.std()) # normalize it
-
-df['atr'] = df.groupby(level=1, group_keys=False).apply(compute_atr)
-
-def compute_macd(close):
-    macd = pandas_ta.macd(close=close, length=20).iloc[:,0]
-    return macd.sub(macd.mean()).div(macd.std()) # normalize it
-
-df['macd'] = df.groupby(level=1, group_keys=False)['adj close'].apply(compute_macd)
-df['dollar_vol'] = (df['adj close']*df['volume'])/1e6 # divide by a million
 
