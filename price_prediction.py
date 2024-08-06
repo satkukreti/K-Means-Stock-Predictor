13db5a1616c05931bfca8a449bec627634927403
satkukreti <satkukreti@gmail.com>
2024-08-05 23:29:36 -0400

    created and benchmarked strategy against SPY

--- price_prediction.py
+++ price_prediction.py
@@ -207,3 +207,76 @@ stocks = data.index.get_level_values('ticker').unique().tolist()
 new_df = yf.download(tickers=stocks,
                      start=data.index.get_level_values('date').unique()[0]-pd.DateOffset(months=12),
                     end=data.index.get_level_values('date').unique()[-1])
+
+# loop over fixed_dates to get optimized weights for stocks, monthly
+# rebalancing every new month
+
+# calculate daily returns for portfolio
+
+returns_df = np.log(new_df['Adj Close']).diff()
+
+portfolio_df = pd.DataFrame()
+
+for start_date in fixed_dates.keys():
+    try:    
+        end_date = (pd.to_datetime(start_date) + pd.offsets.MonthEnd(0)).strftime('%Y-%m-%d')
+        cols = fixed_dates[start_date]
+    
+        # calculate weights using past year info
+        optimization_start_date = (pd.to_datetime(start_date) - pd.DateOffset(months=12)).strftime('%Y-%m-%d')
+        optimization_end_date = (pd.to_datetime(start_date) - pd.DateOffset(days=1)).strftime('%Y-%m-%d')
+    
+        optimization_df = new_df[optimization_start_date:optimization_end_date]['Adj Close'][cols]
+
+        success = False
+        try:
+            weights = optimize_weights(prices=optimization_df,
+                                    lower_bound=round(1/(len(optimization_df.columns)*2),3))
+            weights = pd.DataFrame(weights, index=pd.Series(0))
+            success = True
+        except:
+           print(f'Max Sharpe opt failed for {start_date}, continuing with Equal-Weights')
+
+        if not success:
+            weights = pd.DataFrame([1/len(optimization_df.columns) for i in range(len(optimization_df.columns))],
+                                  index=optimization_df.columns.tolist(),
+                                  columns=pd.Series(0)).T
+        temp_df = returns_df[start_date:end_date]
+        temp_df = temp_df.stack().to_frame('return').reset_index(level=0)\
+                    .merge(weights.stack().to_frame('weight').reset_index(level=0, drop=True),
+                                                                                left_index=True,
+                                                                                right_index=True)\
+                    .reset_index().set_index(['Date', 'index']).unstack().stack()
+        temp_df.index.names = ['date', 'ticker']
+        temp_df['weighted_return'] = temp_df['return']*temp_df['weight']
+        temp_df = temp_df.groupby(level=0)['weighted_return'].sum().to_frame('strategy return')
+        
+        portfolio_df = pd.concat([portfolio_df, temp_df], axis=0)
+
+    except Exception as e:
+        print(e)
+
+portfolio_df = portfolio_df.drop_duplicates()
+
+# benchmark it against sp500
+
+spy = yf.download(tickers='SPY',
+                  start='2015-04-01',
+                  end=dt.date.today())
+spy_ret = np.log(spy[['Adj Close']]).diff().dropna().rename({'Adj Close': 'SPY Buy&Hold'}, axis=1)
+
+portfolio_df = portfolio_df.merge(spy_ret,
+                                  left_index=True,
+                                  right_index=True)
+import matplotlib.ticker as mtick
+
+plt.style.use('ggplot')
+
+portfolio_cumulative_return = np.exp(np.log1p(portfolio_df).cumsum())-1
+
+portfolio_cumulative_return[:'2024-01-30'].plot(figsize=(16,6))
+
+plt.title('Unsupervised Learning Trading Strategy Returns Over Time')
+plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1))
+plt.ylabel('Return')
+plt.show()
